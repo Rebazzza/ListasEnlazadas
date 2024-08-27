3ed0465fef830a8818fe708ab842a845bb70f8ce
ma5ti <ma5ti@REBAZA>
2024-08-26 22:52:45 -0500

    Agregar archivos de proyecto.

---
+++ Sem15005/Lista.cs
@@ -0,0 +1,71 @@
+﻿using Biblioteca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem15005
+{
+   public class Lista
+    {
+        public Nodo primero = null;
+        public void insertar(int num)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.dato = num;
+            if (primero == null)
+            {
+                primero = nuevo;
+            }
+            else
+            {
+                Nodo temp = primero;
+                while (temp.sig  != null )
+                {
+                    temp = temp.sig; 
+                }
+                temp.sig = nuevo;
+            }
+        }
+        public void mostrar()
+        {
+            Nodo temp = primero;
+            while (temp!= null)
+            {
+                Console.Write(temp.dato);
+                if (temp.sig != null)
+                {
+                    Console.Write("->");
+                }
+                        
+               
+                temp = temp.sig;
+            }
+
+            Console.WriteLine(" ");
+
+        }
+        public void buscar(int busc)
+        {
+            Nodo temp = primero;
+            int cont = 0;
+            int espacio = 0;
+            while (temp!=null)
+            {
+                espacio++;
+                if (temp.dato == busc)
+                {
+                    Console.WriteLine("Elemento encontrado en espacio:"+espacio);
+                    cont = 1;
+                }
+                
+                temp = temp.sig;
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("Elemento no encontrado");
+            }
+        }
+    } 
+}
---
+++ Sem15005/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem15005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lista ls = new Lista();
+            int op = 0;
+            ls.insertar(10);
+            ls.insertar(2);
+            ls.insertar(19);
+            ls.insertar(4);
+            ls.insertar(12);
+            ls.insertar(11);
+            while (op != 4)
+            {
+                Console.Clear();
+                Console.WriteLine("------MENÚ------");
+                Console.WriteLine("[1]Mostrar lista");
+                Console.WriteLine("[2]Insertar elemento");
+                Console.WriteLine("[3]Buscar elemento");
+                Console.WriteLine("[4]Salir");
+                Console.WriteLine("----------------");
+                Console.Write("Ingresar opción: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("------LISTA------");
+                        ls.mostrar();
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("Presionar botón para volver");
+                        Console.ReadKey();
+                        
+
+                        break;
+                    case 2:
+                        Console.WriteLine("-----AGREGAR-----");
+                        Console.WriteLine("Agregar número: ");
+                        int nunum = int.Parse(Console.ReadLine());
+                        ls.insertar(nunum);
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("Presionar botón para volver");
+                        Console.ReadKey();
+                        
+                        break;
+                    case 3:
+                        Console.WriteLine("-----BUSCAR-----");
+                        Console.Write("Buscar número:");
+                        int buscar = int.Parse(Console.ReadLine());
+                        ls.buscar(buscar);
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("Presionar botón para volver");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+           
+            
+            
+
+
+
+
+        } 
+    }
+}
